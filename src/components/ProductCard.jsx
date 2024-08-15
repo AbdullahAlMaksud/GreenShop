029c5a5f2eff65062aa0ccdf313a4559b1d38ed6
Abdullah Al Maksud <maksud.workspace@gmail.com>
2024-08-15 23:52:37 +0600

    add hover effect to card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,29 +1,49 @@
 import { Button } from '@material-tailwind/react';
 import React from 'react';
+import { BiDetail } from 'react-icons/bi';
 import { BsStarFill } from 'react-icons/bs';
 import { CiStar } from 'react-icons/ci';
 import { FaStar } from 'react-icons/fa';
 import { FcRating, FcRatings } from 'react-icons/fc';
 import { MdAttachMoney, MdCheck, MdDoneAll } from 'react-icons/md';
 
-const ProductCard = ({product}) => {
+const ProductCard = ({ product }) => {
     return (
-        <div className='shadow-sm rounded-xl hover:shadow-xl hover:shadow-black/20 hover:cursor-pointer'>
-            <div className='bg-secondary/10 rounded-xl relative'>
-                <img src={product.image} className='h-32 lg:h-80 object-contain mx-auto py-2' alt="" />
-                {
-                    product.sale===true ? <span className='absolute text-xs bg-red-400 rounded-xl px-3 text-white top-2 left-2'>Sale</span> : ""
-                }
+        <div className='shadow-sm rounded-xl relative group'>
+
+            <div>
+                <div className='bg-secondary/10 rounded-xl relative'>
+                    <img src={product.image} className='h-32 lg:h-80 object-contain mx-auto py-2' alt="" />
+                    {
+                        product.sale === true ? <span className='absolute text-xs bg-red-400 rounded-xl px-3 text-white top-2 left-2'>Sale</span> : ""
+                    }
+                </div>
+                <div className='pb-2'>
+                    <h2 className='text-center text-md font-semibold mt-2'>{product.name}</h2>
+                    <div className='flex gap-2 justify-center text-sm px-2'>
+                        <p className='flex items-center justify-center rounded border px-1'><MdAttachMoney className='text-green-800' />{product.price}</p>
+                        <p className='flex items-center justify-center border gap-1 px-1 rounded'><MdDoneAll className='text-red-800' /> {product.pricePerUnit}</p>
+                    </div>
+                    <h2 className='text-center text-xs truncate ... mb-2 px-3'>{product.description}</h2>
+                </div>
             </div>
-            <div className='pb-2'>
-            <h2 className='text-center text-md font-semibold mt-2'>{product.name}</h2>
-            <div className='flex gap-2 justify-center text-sm px-2'>
-                <p className='flex items-center justify-center rounded border px-1'><MdAttachMoney className='text-green-800'/>{product.price}</p>
-                <p className='flex items-center justify-center border gap-1 px-1 rounded'><MdDoneAll className='text-red-800'/> {product.pricePerUnit}</p>
+            <div className='absolute h-full w-full bg-green-400/40 items-center flex justify-center -bottom-20 duration-500 scroll-smooth group-hover:bottom-0 opacity-0 group-hover:opacity-100 hover:backdrop-blur-md rounded-xl'>
+                <div className='flex flex-col justify-center items-center px-5 text-center'>
+
+                    <h2 className='font-bold'>{product.name}</h2>
+                    <p className='text-xs'>{product.description}</p>
+                    <div className='bg-white/40 px-5 py-2 rounded-lg border border-border mt-2 text-xs font-sans'>
+                    <p className=''>Arrived: {product.arrivalDate}</p>
+                    <p className=''>Rating: {product.rating}</p>
+                    <p className=''>Price: ${product.price}/{product.pricePerUnit}</p>
+                    </div>
+                    <div className='flex mt-5 gap-2 items-center justify-center'>
+                    <Button className='bg-success max-h-10'>Add to Cart</Button>
+                    <Button className='max-h-10'><BiDetail className='text-lg'/></Button>
+                    </div>
+                </div>
+
             </div>
-            <h2 className='text-center text-xs truncate ... mb-2 px-3'>{product.description}</h2>
-            </div>
-            <Button className='w-full bg-success'>Add to Cart</Button>
         </div>
     );
 };
